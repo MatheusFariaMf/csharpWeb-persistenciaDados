e15bd753d30d736e0d3658082d277e8d2b41c410
MatheusFariaMf <matheus.mfd@hotmail.com>
2024-11-13 19:06:08 -0300

    FinalizacaoCursoWebAPI
    Criação de recursos no Azure: banco de dados e WebAPI, com isso a aplicação pôde ser publicada na web.

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using ScreenSound.API.Endpoints;
 using ScreenSound.DB;
 using ScreenSound.Modelos;
@@ -9,7 +11,11 @@ using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Injeções de dependências
-builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddDbContext<ScreenSoundContext>((options) => {
+    options
+    .UseSqlServer(builder.Configuration["ConnectionStrings:ScreenSoundDB"])
+    .UseLazyLoadingProxies(true);
+});
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
 builder.Services.AddTransient<DAL<Genero>>();
--- ScreenSound.Shared.Dados/DB/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/DB/ScreenSoundContext.cs
@@ -18,8 +18,16 @@ namespace ScreenSound.DB
 
         private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
+        public ScreenSoundContext(DbContextOptions options):base(options) 
+        {
+
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            if (optionsBuilder.IsConfigured)
+                return;
+
             optionsBuilder
                 .UseSqlServer(connectionString)
                 .UseLazyLoadingProxies(true);
